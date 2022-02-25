e73ad165f0600bbb5d9fadf4a1672d716f90856b
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-02-25 20:41:06 +0530

    Update Alerters.c

--- Alerters.c
+++ Alerters.c
@@ -4,17 +4,16 @@ int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
 
-int emailAlerter()
+int8 emailAlerter()
 {
     printf("Max threshold exceeded\n");
     emailAlertCallCount = 1;
     return emailAlertCallCount;
 }
 
-int ledAlertCallCount()
+int8 ledAlerter()
 {
     printf("Max threshold exceeded\n");
     ledAlertCallCount = 1;
     return ledAlertCallCount;
 }
-
